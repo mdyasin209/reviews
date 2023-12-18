f4eb0e15abacc33a5cca27aa365cec8c4797df3c
Yasin <mdyasinjkt209@gmail.com>
2023-12-19 00:18:59 +0600

    create review generate modal

--- reviews.js
+++ reviews.js
@@ -24,3 +24,31 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+// Get the modal
+var modal = document.getElementById("myReviewModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("submitBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close_review_modal")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
